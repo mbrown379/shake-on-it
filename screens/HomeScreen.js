79ebe7d5293ec53f9ae08339da3133929d387b34
Dominguez, Griffin Alexander <gdominguez8@gatech.edu>
2021-10-23 12:05:36 -0400

    Create BetCard

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+import BetCard from '../components/BetCard';
+
+const betsList = [
+    {
+        id: 1,
+        name: 'This is the first bet',
+        users: [
+          {
+            username: "griffindominguez",
+            payout: 129
+          },
+          {
+            username: "matthewbrown",
+            payout: 121
+          }
+        ]
+     }
+]
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
 const HomeScreen = ({ navigation }) => {
-    return null;
-};
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={this.betsList}
+        renderItem={({item})=>
+            <BetCard props={item}>
+            </BetCard>
+        }
+        keyExtractor={item=item.id}
+      />
+    </View>
+  );
+}
 
 export default HomeScreen;
